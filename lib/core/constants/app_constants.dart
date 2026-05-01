1de5902fa3958ce0599a9ae113b63b47d51b54f5
wesly <weslyambarita4@gmail.com>
2026-05-01 16:16:17 +0700

    update

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,14 +1,13 @@
 class AppConstants {
-  // Mengambil URL dari argumen build, defaultnya ke IP lokal jika tidak ada argumen.
-  // Cara run: flutter run --dart-define=BASE_URL=http://192.168.x.x/api
+  // Gunakan 10.0.2.2 karena kamu mendevelop di Emulator Android
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://192.168.x.x/api', // TODO: Sesuaikan dengan IP Nginx
+    defaultValue: 'http://10.0.2.2:8001/api',
   );
 
   // URL untuk Token Keycloak
   static const String keycloakTokenUrl = String.fromEnvironment(
     'KEYCLOAK_URL',
-    defaultValue: 'http://192.168.x.x/auth/realms/smkn1sigumpar/protocol/openid-connect/token',
+    defaultValue: 'http://10.0.2.2:8080/auth/realms/smkn1sigumpar/protocol/openid-connect/token',
   );
 }
